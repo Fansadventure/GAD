e8487e4520a9fbac05f8ff4234c248c2d764b5d4
mathiasj33 <mathias.jackermeier@tum.de>
2017-07-12 14:15:35 +0200

    asd

---
+++ test/com/bitbucket/mathiasj33/gad/blatt06/QuicksortTest.java
@@ -0,0 +1,10 @@
+package com.bitbucket.mathiasj33.gad.blatt06;
+
+import org.junit.Test;
+
+public class QuicksortTest {
+	@Test
+	public void test() {
+		new Quicksort().sort(new int[]{9,8,7,6,5,4,3,2,1,0});
+	}
+}
